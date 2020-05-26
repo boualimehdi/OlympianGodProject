ef1c0d62ae6d59508ccbecd722b74f976d378ae4
boualimehdi <a-mehdi.bouali@outlook.fr>
2020-05-26 17:12:12 +0200

    implementation of the second US

--- src/test/java/Features/US_Kratos_02.feature
+++ src/test/java/Features/US_Kratos_02.feature
@@ -1,6 +1,6 @@
 Feature: US_kratos_02 Aims to test the case of when a human (not Kratos) defies an olympian god
 
-  Scenario : Only the legendary Kratos can beat the god of war Ares
+  Scenario: Only the legendary Kratos can beat the god of war Ares
     Given a human "Deimos" and "Ares" the "god of war"
     And the human is not "Kratos"
     When the human defies Ares
--- src/test/java/Steps/US_Kratos_02_StepDef.java
+++ src/test/java/Steps/US_Kratos_02_StepDef.java
@@ -1,24 +1,38 @@
 package Steps;
 
+import eu.dauphine.sitn.agility.Human;
+import eu.dauphine.sitn.agility.OlympianGod;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class US_Kratos_02_StepDef {
+
+    private Human human;
+    private OlympianGod god;
+
     @Given("a human {string} and {string} the {string}")
-    public void aHumanAndThe(String arg0, String arg1, String arg2) {
+    public void aHumanAndThe(String humanName, String godName, String godAlias) {
+        this.human = new Human(humanName);
+        this.god = new OlympianGod(godName, godAlias);
     }
 
     @And("the human is not {string}")
-    public void theHumanIsNot(String arg0) {
+    public void theHumanIsNot(String kratosString) {
+        Assert.assertFalse(this.human.getName().equalsIgnoreCase(kratosString));
     }
 
     @When("the human defies Ares")
     public void theHumanDefiesAres() {
+        this.human.defy(this.god);
     }
 
     @Then("Ares beat him and keep his title")
     public void aresBeatHimAndKeepHisTitle() {
+        Assert.assertFalse(this.god.getMortality());    //the god is still alive
+        Assert.assertFalse(this.god.isHuman());         //the god is not a human
+        Assert.assertFalse(this.human.isGod());         //the human did not become a god
     }
 }
